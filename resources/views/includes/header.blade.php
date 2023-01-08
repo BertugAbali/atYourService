44655a528c6765be5400c1eab30cb414d96342a7
Bertuğ Abalı <bertugabali@hotmail.com>
2023-01-08 16:38:00 +0000

    logo

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,8 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            At Your Service
+        <img src="{{asset('storage/images/logo/logo.PNG')}}" width="50" height="50" alt="">
+ At Your Service
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
